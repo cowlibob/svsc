d8434653afe7828a292fb59f6de5806a6a4148bb
James Cowlishaw <james@cowlibob.co.uk>
2013-03-17 21:51:44 +0000

    local changes.

--- config.rb
+++ config.rb
@@ -1,13 +1,11 @@
-require 'sass-globbing'
-
 # Require any additional compass plugins here.
 project_type = :stand_alone
 
 # Publishing paths
-http_path = "/"
-http_images_path = "/images"
-http_fonts_path = "/fonts"
-css_dir = "public/stylesheets"
+http_path = "/svsc/"
+http_images_path = "/svsc/images"
+http_fonts_path = "/svsc/fonts"
+css_dir = "public/svsc/stylesheets"
 
 # Local development paths
 sass_dir = "sass"
